6b8694f0955ab9b241eb3d82ca54b447c1e26bc1
art <art_jr2000@yahoo.com>
2017-08-14 16:57:26 -0500

    created js files -added html -added js folder -added alerts, promts, console log

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict"
+
+console.log('Hello From External Javascript');
+
+alert('Welcome to My Website');
+
+var userInput = prompt('What is your favorite color?');
+alert('Great ' + userInput + ' thats my favorite color too');
